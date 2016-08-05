efcda897212642a1f9717e0f34703bb97a3f1fa6
eduardocastaneda <eduardo.castaneda@valtech.se>
2016-08-05 13:04:00 +0200

    gulpfile changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,15 @@
-var gulp = require('gulp')
-var sass = require('gulp-ruby-sass')
-var connect = require('gulp-connect')
-var browserify = require('browserify')
-var source = require('vinyl-source-stream')
+var gulp = require('gulp');
+var sass = require('gulp-ruby-sass');
+var connect = require('gulp-connect');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
 
 gulp.task('connect', function () {
   connect.server({
     root: 'public',
     port: 4000
-  })
-})
+  });
+});
 
 gulp.task('browserify', function() {
   // Grabs the app.js file
@@ -19,16 +19,16 @@ gulp.task('browserify', function() {
         .pipe(source('main.js'))
         // saves it the public/js/ directory
         .pipe(gulp.dest('./public/js/'));
-})
+});
 
 gulp.task('sass', function() {
   return sass('style/main.scss')
-    .pipe(gulp.dest('public/css'))
-})
+    .pipe(gulp.dest('public/css'));
+});
 
 gulp.task('watch', function() {
-  gulp.watch('app/**/*.js', ['browserify'])
-  gulp.watch('style/**/*.scss', ['sass'])
-})
+  gulp.watch('app/**/*.js', ['browserify']);
+  gulp.watch('style/**/*.scss', ['sass']);
+});
 
-gulp.task('default', ['connect', 'browserify', 'sass', 'watch'])
+gulp.task('default', ['connect', 'browserify', 'sass', 'watch']);
